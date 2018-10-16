2501d9c8274d47979518f1ca895e2706b4eaba6e
Miguel Ping <miguel.ping@indiecampers.com>
2018-10-16 10:03:10 +0100

    allow for unsigned releases until gpg is setup

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   optionally with postgres/hikari/migratus, selmer and friend-oauth2"
   :url "https://github.com/mping/compojure-api-chassis-template"
   :license {:name "MIT License" :url "http://opensource.org/licenses/MIT"}
+  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/repo"}]
+                        ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]
   :scm {:name "git" :url "https://github.com/mping/compojure-api-chassis-template.git"}
   :eval-in-leiningen true)
